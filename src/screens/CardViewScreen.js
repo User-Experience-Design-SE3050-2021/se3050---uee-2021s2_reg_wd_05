740994408bbd5ffabab79ba1a758f8a363e1839f
Zumry <amzumry@gmail.com>
2021-10-09 20:37:23 +0530

    scroll view added and navigation bar error fixed

--- src/screens/CardViewScreen.js
+++ src/screens/CardViewScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, StyleSheet, ImageBackground, Image, Dimensions, TouchableOpacity, Text} from "react-native";
+import {View, StyleSheet, ImageBackground, Image, Dimensions, TouchableOpacity, Text, ScrollView} from "react-native";
 import {NavigationBar} from "../components";
 
 const {width} = Dimensions.get('window');
@@ -11,78 +11,84 @@ const CardViewScreen = ({navigation}) => {
     }
 
     return (
-      <View style={styles.mainContainer}>
+        <View style={styles.mainContainer}>
+            <ScrollView bounces={false} showsVerticalScrollIndicator={false} style={styles.scrollContainer}>
+                <View style={styles.inputContainer}>
+                    <ImageBackground
+                        source={require('../assets/images/Visa_Card.jpg')}
+                        style={styles.cards}>
+                        {/*<InputField text="Card Holder Name" />*/}
+                        <Text style={styles.textType}>Credit</Text>
+                        <Text style={styles.textName}>Zayan Malik</Text>
+                        <Text style={styles.text}>5142 - XXXX - XXXX - 2563</Text>
+                    </ImageBackground>
+                </View>
 
-          <View style={styles.inputContainer}>
-              <ImageBackground
-                source={require('../assets/images/Visa_Card.jpg')}
-                style={styles.cards}>
-                  {/*<InputField text="Card Holder Name" />*/}
-                  <Text style={styles.textType}>Credit</Text>
-                  <Text style={styles.textName}>Zayan Malik</Text>
-                  <Text style={styles.text}>5142 - XXXX - XXXX - 2563</Text>
-              </ImageBackground>
-          </View>
+                <View style={styles.addButtonContainer}>
+                    <TouchableOpacity style={styles.iconContainer}
+                                      onPress={() => navigation.navigate('EnterCardDetails')}>
+                        <Image source={require('../assets/images/Add_Button.png')}
+                               style={{width: 50, height: 50,}} onPress={btnClick}/>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+            <View style={styles.bottomContainer}>
+                <NavigationBar navigation={navigation}/>
+            </View>
 
-          <View style={styles.addButtonContainer}>
-              <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('EnterCardDetails')}>
-                  <Image source={require('../assets/images/Add_Button.png')}
-                         style={{width: 50, height: 50,}} onPress={btnClick}/>
-              </TouchableOpacity>
-          </View>
-
-          <View style={styles.bottomContainer}>
-              <NavigationBar navigation={navigation}/>
-          </View>
-
-      </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
+        flex: 1
+    },
+    scrollContainer: {
+        flex: 1,
         padding: 10,
     },
     inputContainer: {
         height: 'auto',
         backgroundColor: 'white',
         padding: 10,
+        marginBottom: 10,
         alignItems: 'center',
         borderRadius: 10,
+    },
+    bottomContainer: {
+        paddingTop: 20,
+        alignItems: 'center'
     },
     cards: {
         width: 380,
         height: 200,
     },
-    textType:{
-        fontSize:20,
+    textType: {
+        fontSize: 20,
         paddingTop: 28,
-        paddingLeft:20,
-        marginRight:'auto',
+        paddingLeft: 20,
+        marginRight: 'auto',
         color: 'white',
     },
-    text:{
-        fontSize:18,
+    text: {
+        fontSize: 18,
         paddingTop: 10,
-        paddingLeft:20,
-        marginRight:'auto',
+        paddingLeft: 20,
+        marginRight: 'auto',
         color: 'white',
     },
-    textName:{
-        fontSize:18,
+    textName: {
+        fontSize: 18,
         paddingTop: 70,
         paddingLeft: 20,
-        marginRight:'auto',
+        marginRight: 'auto',
         color: 'white',
     },
     addButtonContainer: {
         paddingTop: 225,
         marginLeft: width / 1.25,
     },
-    bottomContainer: {
-        paddingTop: 40,
-        alignItems: 'center'
-    }
 })
 
 export default CardViewScreen;
